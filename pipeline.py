3e949bdd230823c4db6a4c859be26ea8d17adc6c
Foundry (vector-client-token) <>
2022-12-14 02:51:12 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -53,12 +53,10 @@ def cci_count(icd_match, concept_set_members, Long_COVID_Silver_Standard_train, 
     mapped_concepts=Input(rid="ri.foundry.main.dataset.313bf22e-6ba2-46a6-be7b-742db516104c")
 )
 def condition_mapped(mapped_concepts, condition_era_train, condition_era):
-    return mapped_concepts
     #Join test/train for condition_eras
-    # condition_train_test = condition_era_train.unionByName(condition_era, allowMissingColumns=True)
-    # return condition_train_test
-    # condition_train_test = condition_train_test.dropDuplicates(['condition_era_id']) #DELETE FOR FINAL
-    # return condition_train_test.join(mapped_concepts, condition_train_test.condition_concept_id==mapped_concepts.concept_id_2,'left')
+    condition_train_test = condition_era_train.unionByName(condition_era, allowMissingColumns=True)
+    condition_train_test = condition_train_test.dropDuplicates(['condition_era_id']) #DELETE FOR FINAL
+    return condition_train_test.join(mapped_concepts, condition_train_test.condition_concept_id==mapped_concepts.concept_id_2,'left')
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.cb07b6ff-9f7a-4fbe-9769-bdbbc922fc9d"),

50bbb5996fb2cee8be3a67ee75cd20d57b73ad4a
Foundry (vector-client-token) <>
2022-12-14 16:10:47 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -4,7 +4,6 @@
     Output(rid="ri.vector.main.execute.afad3330-c701-4b0b-9ee1-0922868e898b"),
     microvisits_to_macrovisits_train=Input(rid="ri.foundry.main.dataset.d77a701f-34df-48a1-a71c-b28112a07ffa")
 )
-SELECT distinct visit_concept_name
+SELECT count(distinct visit_occurrence_id)
 FROM microvisits_to_macrovisits_train
-where macrovisit_id is not null
 

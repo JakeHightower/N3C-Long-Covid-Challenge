ececf88a0f8ebbf4baff1a2383e16fbd4bc708c3
Foundry (vector-client-token) <>
2022-12-18 00:05:25 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -442,8 +442,8 @@ def pivot_by_person(cci_count):
     xgb_hyperparam_tuning=Input(rid="ri.foundry.main.dataset.cec62123-8cbd-42a8-8f20-cd5a18438cff")
 )
 def unnamed(xgb_hyperparam_tuning):
-    return xgb_hyperparam_tuning.melt()
-    # return xgb_hyperparam_tuning.T
+    print xgb_hyperparam_tuning.melt().to_dict()
+    return
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.cec62123-8cbd-42a8-8f20-cd5a18438cff"),

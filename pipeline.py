f41ea36255cd3276a504ae060c4be506e5dda044
Foundry (vector-client-token) <>
2023-02-16 20:58:38 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -454,13 +454,13 @@ def ruvos_predictions(xgb_hyperparam_tuning, model_prep, person):
     plt.style.use('default')
     #gain
     plt.rcParams["figure.figsize"] = (5,30)
-    plot_importance(model, max_num_features=30, importance_type="gain", grid=False, xlabel="Average gain", show_values=False)
+    plot_importance(model, max_num_features=50, importance_type="gain", grid=False, xlabel="Average gain", show_values=False)
     #
     plt.tight_layout()
     plt.show()
 
     #Weight
-    plot_importance(model, max_num_features=30, grid=False, xlabel="Weight", show_values=False)
+    plot_importance(model, max_num_features=50, grid=False, xlabel="Weight", show_values=False)
     #plt.rcParams["figure.figsize"] = (45,5)
     plt.tight_layout()
     plt.show()
